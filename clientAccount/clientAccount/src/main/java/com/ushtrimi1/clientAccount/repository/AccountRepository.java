61894be24ba6c6fbb27eb8f8113b9a8e3cc0e256
FACILIZATION\fiorilda.bushi <fiorildabushi8@gmail.com>
2022-09-16 16:06:04 +0200

    no message

--- clientAccount/clientAccount/src/main/java/com/ushtrimi1/clientAccount/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ushtrimi1.clientAccount.repository;
-
-import com.ushtrimi1.clientAccount.model.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account,Long> {
-
-}
