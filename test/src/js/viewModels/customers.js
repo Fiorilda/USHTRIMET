cc17f3ce91425c3ed6d58600586dfda572d85a5a
FACILIZATION\fiorilda.bushi <fiorildabushi8@gmail.com>
2022-09-23 17:18:35 +0200

    Oracle JET

--- test/src/js/viewModels/customers.js
+++ test/src/js/viewModels/customers.js
@@ -20,7 +20,7 @@ define([
         "ojs/ojselectsingle",
         "ojs/ojbutton",
         "ojs/ojtable",
-        
+        "ojs/ojchart"   
       ],
  function(
     ko,
@@ -34,7 +34,7 @@ define([
       this._initAllLabels();
       this._initAllObservables();
     };
-
+    
     CustomerViewModel.prototype._initAllObservables=function (){
       this.inputFirstNameValue=ko.observable(null);
       this.inputLastNameValue=ko.observable(null);
@@ -43,12 +43,56 @@ define([
       this.inputBirthDateValue=ko.observable(null);
       this.inputGenderValue=ko.observable(null);
       
-     this.deptArray = JSON.parse(null);
+        this.deptArray = JSON.parse(null);
               this.deptObservableArray = ko.observableArray(this.deptArray);
               this.dataprovider = new BufferingDataProvider(new ArrayDataProvider(this.deptObservableArray, {
                   keyAttributes: 'Name'
       }));
 
+      this.dataProvider1=ko.observable();
+
+      //this.NRFemer;
+      this.array=[
+        {
+          id: 0,
+          series: "Male",
+          group: "Group A",
+          value: 23
+        }, 
+        {
+          id: 1,
+          series: "Female",
+          group: "Group A",
+          value: 55
+        }
+      ];
+
+      this.deptObservableArray = ko.observableArray(this.array);
+      this.dataProvider1= new ArrayDataProvider(this.deptObservableArray, {
+        keyAttributes: "id",
+      });
+
+      console.log(this.array[1].value);
+      console.log(this.NRFemer);
+
+      // this.dataProvider1=ko.observable( new ArrayDataProvider([
+      //   {
+      //     id: 0,
+      //     series: "Series 1",
+      //     group: "Group A",
+      //     value: this.inputAgeValue()
+      //   }, 
+      //   {
+      //     id: 1,
+      //     series: "Series 2",
+      //     group: "Group A",
+      //     value: 55
+      //   }
+      // ], {
+      //     keyAttributes: "label",
+      // }));
+
+      
       //Data Provider
       this.inputGenderDataProvider=ko.observable( new ArrayDataProvider([
         {
@@ -64,6 +108,7 @@ define([
       }));
 
       this.onSaveButtonClick=this._onSaveButtonClick.bind(this);
+
  };
 
     CustomerViewModel.prototype._onSaveButtonClick=function (){
@@ -79,7 +124,31 @@ define([
         metadata: { key: ClientData.Name},  
         data:ClientData
       });
-     
+
+      
+      // console.log(ClientData.Gender);
+      //    if(ClientData.Gender=='Female'){
+      //       this.NRFemer++;
+      //    }
+      // console.log("nr femer"+this.NRFemer);
+
+      // this.array[1].value=this.NRFemer;
+      // console.log(this.array[1].value);
+
+      // const array=[
+      //   {
+      //     id: 0,
+      //     series: "Series 1",
+      //     group: "Group A",
+      //     value: this.inputAgeValue()
+      //   }, 
+      //   {
+      //     id: 1,
+      //     series: "Series 2",
+      //     group: "Group A",
+      //     value: 55
+      //   }
+      // ]; 
     };
 
     CustomerViewModel.prototype._initAllIds=function (){
